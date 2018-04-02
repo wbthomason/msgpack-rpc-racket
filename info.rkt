87276b37755385794d46d42860e7a611faa930c4
Wil Thomason <wil.thomason@gmail.com>
2018-04-02 14:03:19 -0400

    Very WIP defining the module interface

--- info.rkt
+++ info.rkt
@@ -1,8 +1,8 @@
 #lang info
 (define collection "msgpack-rpc")
-(define deps '("base"))
+(define deps '("base" "msgpack" "unix-socket-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/msgpack-rpc.scrbl" ())))
-(define pkg-desc "Description Here")
-(define version "0.0")
-(define pkg-authors '(wil))
+(define pkg-desc "MsgPack RPC Client in Racket")
+(define version "0.1.0")
+(define pkg-authors '(wbthomason))
--- main.rkt
+++ main.rkt
@@ -1,35 +1,11 @@
 #lang racket/base
 
-(module+ test
-  (require rackunit))
+(require msgpack)
 
-;; Notice
-;; To install (from within the package directory):
-;;   $ raco pkg install
-;; To install (once uploaded to pkgs.racket-lang.org):
-;;   $ raco pkg install <<name>>
-;; To uninstall:
-;;   $ raco pkg remove <<name>>
-;; To view documentation:
-;;   $ raco docs <<name>>
-;;
-;; For your convenience, we have included a LICENSE.txt file, which links to
-;; the GNU Lesser General Public License.
-;; If you would prefer to use a different license, replace LICENSE.txt with the
-;; desired license.
-;;
-;; Some users like to add a `private/` directory, place auxiliary files there,
-;; and require them in `main.rkt`.
-;;
-;; See the current version of the racket style guide here:
-;; http://docs.racket-lang.org/style/index.html
+(provide start-client
+         stop-client
+         rpc-call)
 
-;; Code here
+;; Datatypes
 
-(module+ test
-  ;; Tests to be run with raco test
-  )
 
-(module+ main
-  ;; Main entry point, executed when run with the `racket` executable or DrRacket.
-  )
