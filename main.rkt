2f47323d0fdc1f630cf9ec5815a2409d4aabef3d
Wil Thomason <wil.thomason@gmail.com>
2018-04-02 23:59:19 -0400

    WIP finishing the client

--- main.rkt
+++ main.rkt
@@ -4,7 +4,8 @@
 
 (provide start-client
          stop-client
-         rpc-call)
+         rpc-request
+         rpc-notify)
 
 ;; Datatypes and internal constructors
 (struct request (type msgid method params))
@@ -16,16 +17,23 @@
 (struct response (type msgid err result))
 ;;; We assume that the server only gives us good responses, which is almost certainly a terrible
 ;;; idea
-(define (make-response-handler callback)
-  (lambda (data)
-    (match data
-           [(response 1 msgid err result) (callback err result)])))
+(define (make-response-handler)
+  (let ([callback-channel (make-channel)])
+    (define (response-handler data)
+      (match data
+             [(response 1 msgid err result) (channel-put callback-channel (values err result))]))))
 
 (struct notify (type method params))
+(define (make-notify method params)
+  (let ([data #(2 method params)])
+    (call-with-output-bytes (lambda (out) (pack data out)))))
 
 ;; Making a call
-(define (rpc-call client callback method [sync? #t] . args)
-  (send client (if sync? sync-call async-call) callback method args))
+(define (rpc-request client method [sync? #t] . args)
+  (send client (if sync? sync-request async-request) method args))
+
+(define (rpc-notify client method [sync? #t] . args)
+  (send client (if sync? sync-notify async-notify) method args))
 
 ;; The RPC Client class
 ;;; TODO All methods that require the client to be started should check that it has been started
@@ -70,4 +78,19 @@
                                                    (lambda (evt) (dispatch-response (unpack evt)))))
                                      (loop)))))))
          (define/private (dispatch-response data)
-           ())))
+           ((hash-ref pending-requests (vector-ref data 1)) data))
+         (define/private (send-request method args)
+           (let-values ([request-bytes (make-request next-id-num method args)]
+                        [(callback-channel callback-handler) (make-response-handler)])
+             (hash-set! pending-requests next-id-num callback-handler)
+             (next-id)
+             (display request-bytes out)
+             (callback-channel)))
+         (define/public (sync-request method args)
+           (let ([callback-channel (send-request method args)])
+             (channel-get callback-channel)))
+         (define/public (async-request method args)
+           (let ([callback-channel (send-request method args)])
+             (callback-channel)))
+         (define/public (sync-notify method args)
+           (display (make-notify method args) out))))
